41791a912a043986de3c2031d69d1ef60f29e9b3
VictoriaMrkvch <markevuch.viktorija@gmail.com>
2023-12-19 17:49:07 +0200

    add CustomGlobalExceptionHandler class

--- src/main/java/book/store/intro/dto/book/CreateBookRequestDto.java
+++ src/main/java/book/store/intro/dto/book/CreateBookRequestDto.java
@@ -1,21 +1,30 @@
 package book.store.intro.dto.book;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
+    @Length(max = 255)
     private String title;
-    @NotNull
+    @NotBlank
+    @Length(max = 255)
     private String author;
-    @NotNull
+    @NotBlank
+    @Length(max = 255)
     private String isbn;
     @NotNull
     @Min(0)
     private BigDecimal price;
+    @NotNull
+    @Length(max = 255)
     private String description;
+    @NotNull
+    @Length(max = 255)
     private String coverImage;
 }
